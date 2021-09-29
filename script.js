d7f2f084f919c6a1d62941e2d74054864030ece5
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2021-09-29 20:07:09 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+var canConstruct = function(ransomNote, magazine) {
+    //est magazine character map through set and get?
+    const map = new Map();
+    for (let character of magazine){
+          map.set(character, map.get(character) + 1 || 1)
+    }
+    // console.log("map", map)
+    for (let character of ransomNote){
+        if (!map.get(character)) 
+            {return false}
+        map.set(character, map.get(character) - 1 )
+        map[ransomNote[character]]--;
+          console.log("character", character, "value", map.get(character))
+            
+    }
+    return true;   
+};
