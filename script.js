b146ea8489d3340c5101d5fdd3eadf873355af4b
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2021-09-29 20:53:37 +0000

    First Leetcode practice commit

--- script.js
+++ script.js
@@ -3,6 +3,10 @@
  * @param {string} magazine
  * @return {boolean}
  */
+// Leetcode: Instructions: Given two stings ransomNote and magazine, return true if ransomNote can be constructed from magazine and false otherwise.
+//  Each letter in magazine can only be used once in ransomNote.
+
+ 
 Input: ransomNote = "aa", magazine = "aab"
 Output: true
 var canConstruct = function(ransomNote, magazine) {
@@ -11,13 +15,13 @@ var canConstruct = function(ransomNote, magazine) {
     for (let character of magazine){
           map.set(character, map.get(character) + 1 || 1)
     }
-    // console.log("map", map)
+
     for (let character of ransomNote){
         if (!map.get(character)) 
             {return false}
         map.set(character, map.get(character) - 1 )
         map[ransomNote[character]]--;
-          console.log("character", character, "value", map.get(character))
+          // console.log("character", character, "value", map.get(character))
             
     }
     return true;   
